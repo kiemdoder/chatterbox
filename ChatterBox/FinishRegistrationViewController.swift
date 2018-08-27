881e8832267ce101559b644866226c1b116ca235
Roelf de Kock <kiemdoder@gmail.com>
2018-08-27 21:11:15 +0200

    added PodFile

---
+++ ChatterBox/FinishRegistrationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FinishRegistrationViewController.swift
+//  ChatterBox
+//
+//  Created by Roelf De Kock on 2018/08/08.
+//  Copyright © 2018 Draadkar. All rights reserved.
+//
+
+import UIKit
+
+class FinishRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
